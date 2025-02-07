611b45406e1447e09d6d4f1094e06371f51009c3
MOchmanski <mochmanski@mail.pima.edu>
2025-02-06 19:04:53 -0700

    Update cis129_lab02_shortStory.py
    Added a five sentence short story about alien life.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+''' Author: Michael Ochmanski
+CIS129 course Lab 02
+The following segment of code prints a short story of my creation
+'''
+
+print('The Fermi Paradox is a blanket term for any theory that attempts to')
+print('explain if the universe is so vast, water is so plentiful, and there') 
+print('are so many planets, why we haven\'t seen any aliens. One of the')
+print('solutions, The Dark Forest Theory, equates our universe to a dark')
+print('forest where predators and hunters with overwhelming power prowl.')
+print('The dark forest is also has dwindling natural resources that can\'t')
+print('support every predator and hunter that roams free in the forest.')
+print('Screaming into this dark forest for help would reveal you to these')
+print('creatures who will do everything in their power to get rid of you')
+print('in order preserve their resources. Self-preservation instinct leads')
+print('all intelligent alien civilizations to avoid attraction.')
